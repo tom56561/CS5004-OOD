b58bd2f3ec261a0dc0d4ec0ad9a6cd6f6fecd50d
tom56561 <tom56561@gmail.com>
2022-10-03 15:49:17 -0400

    all function

--- homework02/src/Rectangle.java
+++ homework02/src/Rectangle.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 
 /**
  * This class represents a Rectangle. A Rectangle has a x and a y.
@@ -17,7 +18,7 @@ public class Rectangle {
    * @param h the height of the Rectangle
    */
   public Rectangle(int x, int y, int w, int h) throws IllegalArgumentException {
-    if (w < 0 || h < 0) {
+    if (w <= 0 || h <= 0) {
       throw new IllegalArgumentException("width or height should not be negative");
     }
     this.x = x;
@@ -27,7 +28,58 @@ public class Rectangle {
   }
 
   /**
-   * The Vector3D toString is represented by the components of the Vector3D.
+   * Return true if this rectangle overlaps with other, false otherwise.
+   * 
+   * @param other the other Rectangle object
+   * @return true if this rectangle overlaps with other, false otherwise
+   */
+  public boolean overlap(Rectangle other) {
+    return this.x < other.x + other.w && other.x < this.x + this.w && this.y < other.y + other.h
+        && other.y < this.y + this.h;
+  }
+
+  /**
+   * Return a Rectangle object that represents the union of this rectangle and the
+   * other rectangle.
+   * 
+   * @param other the other Rectangle object
+   * @return a Rectangle object that represents the union of this rectangle and
+   *         the other rectangle
+   */
+  public Rectangle intersect(Rectangle other) throws NoSuchElementException {
+    if (this.overlap(other) == false) {
+      throw new NoSuchElementException();
+    }
+    int xL = Math.max(this.x, other.x);
+    int xR = Math.min(this.x + this.w, other.x + other.w);
+    int yB = Math.max(this.y, other.y);
+    int yT = Math.min(this.y + this.h, other.y + other.h);
+
+    return new Rectangle(xL, yB, xR - xL, yT - yB);
+  }
+
+  /**
+   * Return a Rectangle object that represents the union of this rectangle and the
+   * other rectangle.
+   * 
+   * @param other the other Rectangle object
+   * @return a Rectangle object that represents the union of this rectangle and
+   *         the other rectangle
+   */
+  public Rectangle union(Rectangle other) {
+    int xL = Math.min(this.x, other.x);
+    int xR = Math.max(this.x + this.w, other.x + other.w);
+    int yB = Math.min(this.y, other.y);
+    int yT = Math.max(this.y + this.h, other.y + other.h);
+
+    return new Rectangle(xL, yB, xR - xL, yT - yB);
+
+  }
+
+  /**
+   * The Rectangle toString is represented by the components of the Rectangle.
+   * 
+   * @return components of the Rectangle as a string
    */
   public String toString() {
     return String.format("x:%d, y:%d, w:%d, h:%d", this.x, this.y, this.w, this.h);
--- homework02/test/RectangleTest.java
+++ homework02/test/RectangleTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 import org.junit.Test;
@@ -22,12 +23,121 @@ public class RectangleTest {
   }
 
   /**
-   * Tests if input negative width or height Rectangle construct will throw
+   * Tests if input negative width or height, Rectangle construct will throw
    * IllegalArgumentException.
    */
   @Test(expected = IllegalArgumentException.class)
   public void testIllegalRectangle() {
     Rectangle firstRectangle = new Rectangle(2, 3, -4, 5);
+  }
+
+  /**
+   * Tests if overlap properly returns the boolean.
+   */
+  @Test
+  public void testOverlap() {
+
+    Rectangle first;
+    Rectangle other;
+    first = new Rectangle(0, 0, 5, 5);
+    other = new Rectangle(-3, 0, 2, 3);
+    assertEquals(false, first.overlap(other));
+    other = new Rectangle(-3, 0, 4, 3);
+    assertEquals(true, first.overlap(other));
+    other = new Rectangle(-3, 0, 3, 3);
+    assertEquals(false, first.overlap(other));
+
+    boolean expected;
+    Random random = new Random(12345);
+
+    for (int test = 0; test < 5000; test++) {
+      int x = random.nextInt();
+      int y = random.nextInt();
+      int w = Math.abs(random.nextInt());
+      int h = Math.abs(random.nextInt());
+      int ax = random.nextInt();
+      int ay = random.nextInt();
+      int aw = Math.abs(random.nextInt());
+      int ah = Math.abs(random.nextInt());
+      first = new Rectangle(x, y, w, h);
+      other = new Rectangle(ax, ay, aw, ah);
+      expected = x < ax + aw && ax < x + w && y < ay + ah && ay < y + h;
+      assertEquals(expected, first.overlap(other));
+    }
+  }
+
+  /**
+   * Tests if intersect properly returns Rectangle object.
+   */
+  @Test
+  public void testIntersect() {
+
+    Rectangle first;
+    Rectangle other;
+    first = new Rectangle(0, 0, 2, 5);
+    other = new Rectangle(0, -2, 4, 3);
+    assertEquals("x:0, y:0, w:2, h:1", first.intersect(other).toString());
+
+    String expected;
+    Random random = new Random(12345);
+
+    for (int test = 0; test < 5000; test++) {
+      int x = random.nextInt();
+      int y = random.nextInt();
+      int w = Math.abs(random.nextInt());
+      int h = Math.abs(random.nextInt());
+      int ax = random.nextInt();
+      int ay = random.nextInt();
+      int aw = Math.abs(random.nextInt());
+      int ah = Math.abs(random.nextInt());
+      first = new Rectangle(x, y, w, h);
+      other = new Rectangle(ax, ay, aw, ah);
+      int xL = Math.max(x, ax);
+      int xR = Math.min(x + w, ax + aw);
+      int yB = Math.max(y, ay);
+      int yT = Math.min(y + h, ay + ah);
+      expected = String.format("x:%d, y:%d, w:%d, h:%d", xL, yB, xR - xL, yT - yB);
+      try {
+        assertEquals(expected, first.intersect(other).toString());
+      } catch (NoSuchElementException e) {
+      }
+    }
+  }
+
+  /**
+   * Tests if union properly returns Rectangle object.
+   */
+  @Test
+  public void testUnion() {
+
+    Rectangle first;
+    Rectangle other;
+    first = new Rectangle(0, 0, 10, 5);
+    other = new Rectangle(5, 2, 5, 5);
+    assertEquals("x:0, y:0, w:10, h:7", first.union(other).toString());
+
+    String expected;
+    Random random = new Random(12345);
+
+    for (int test = 0; test < 1000; test++) {
+      int x = random.nextInt(1000);
+      int y = random.nextInt(1000);
+      int w = random.nextInt(1000) + 1;
+      int h = random.nextInt(1000) + 1;
+      int ax = random.nextInt(1000);
+      int ay = random.nextInt(1000);
+      int aw = random.nextInt(1000) + 1;
+      int ah = random.nextInt(1000) + 1;
+
+      first = new Rectangle(x, y, w, h);
+      other = new Rectangle(ax, ay, aw, ah);
+      int xL = Math.min(x, ax);
+      int xR = Math.max(x + w, ax + aw);
+      int yB = Math.min(y, ay);
+      int yT = Math.max(y + h, ay + ah);
+      expected = String.format("x:%d, y:%d, w:%d, h:%d", xL, yB, xR - xL, yT - yB);
+      assertEquals(expected, first.union(other).toString());
+    }
   }
 
   /**
