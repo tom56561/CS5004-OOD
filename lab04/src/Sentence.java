6ace1c719a74744e8330c7f992b0cf5a2f9c093a
tom56561 <tom56561@gmail.com>
2022-10-11 14:13:37 -0400

    internface

--- lab04/src/Sentence.java
+++ lab04/src/Sentence.java
@@ -1,11 +1,30 @@
 /**
- * The Sentence interface defines all the supported operations
- * for a Sentence. A Sentence is a list of different elements,
- * which can include Words and Punctuation.
+ * The Sentence interface defines all the supported operations for a Sentence. A
+ * Sentence is a list of different elements, which can include Words and
+ * Punctuation.
  * 
  * @author eddie
  *
  */
 public interface Sentence {
 
+  /**
+   * Return the number of words in a sentence.
+   * The punctuation does not count as a word.
+   * 
+   * @return the number of words in a sentence
+   */
+  int getNumberOfWords();
+
+  /**
+   * Returns the longest word in a sentence. 
+   * Only the word is returned, no
+   * punctuation is included. 
+   * If this sentence contains no words, an empty string is returned.
+   * 
+   * @return the longest word in a sentence
+   */
+  String longestWord();
+
+  Sentence clone();
 }
