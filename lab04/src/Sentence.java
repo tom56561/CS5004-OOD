bb6bcbc17b18a5496a546e21137c0aeb52ba3f2e
tom56561 <tom56561@gmail.com>
2022-10-11 14:09:19 -0400

    Sentence interface

---
+++ lab04/src/Sentence.java
@@ -0,0 +1,11 @@
+/**
+ * The Sentence interface defines all the supported operations
+ * for a Sentence. A Sentence is a list of different elements,
+ * which can include Words and Punctuation.
+ * 
+ * @author eddie
+ *
+ */
+public interface Sentence {
+
+}
