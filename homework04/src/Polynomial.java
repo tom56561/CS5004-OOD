e50d5b3a455b13d90564791a7e45498dc988c245
tom56561 <tom56561@gmail.com>
2022-10-31 15:31:48 -0400

    finished Polynomial interface

--- homework04/src/Polynomial.java
+++ homework04/src/Polynomial.java
@@ -1,4 +1,61 @@
-
+/**
+ * The Polynomial interface defines all the supported operations for each
+ * Polynomial.
+ * 
+ * @author eddie
+ *
+ */
 public interface Polynomial {
+
+  /**
+   * Takes a coefficient and a power (both integer numbers) and adds the resulting
+   * term to the polynomial.
+   * 
+   * @param coef the coefficient
+   * @param pow  the power
+   * @throws IllegalArgumentException if a negative power is passed to it
+   */
+  void addTerm(int coef, int pow) throws IllegalArgumentException;
+
+  /**
+   * Takes a power and removes any and all terms in the polynomial with that
+   * power.
+   * 
+   * @param pow the power
+   */
+  void removeTerm(int pow);
+
+  /**
+   * Returns the degree of this polynomial.
+   * 
+   * @return the degree of this polynomial
+   */
+  int getDegree();
+
+  /**
+   * Takes a power and returns the coefficient for the term with that power.
+   * 
+   * @param pow the power
+   * @return the coefficient for the term with that power
+   */
+  int getCoefficient(int pow);
+
+  /**
+   * Takes a double-precision decimal number and returns a double-precision
+   * result.
+   * 
+   * @param x the x is a double-precision decimal number
+   * @return a double-precision result
+   */
+  double evaluate(double x);
+
+  /**
+   * Takes another Polynomial object and returns the polynomial obtained by adding
+   * the two polynomials.
+   * 
+   * @param other the another Polynomial object
+   * @return the polynomial obtained by adding the two polynomials
+   */
+  PolynomialImpl add(PolynomialImpl other) throws IllegalArgumentException;
 
 }
