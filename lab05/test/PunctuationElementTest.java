5e4bde06900ba5539c6631e992fc274dbb3cc268
tom56561 <tom56561@gmail.com>
2022-11-08 17:11:14 -0500

    finished pwElement test

--- lab05/test/PunctuationElementTest.java
+++ lab05/test/PunctuationElementTest.java
@@ -1,16 +1,35 @@
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
+/**
+ * Test PunctuationElementTest method.
+ * 
+ * @author eddie
+ *
+ */
 public class PunctuationElementTest {
 
-//  @Test
-//  public void test() {
-//    
-//    PunctuationElement p = new PunctuationElement("!");
-//    assertTrue()
-//  }
+  /**
+   * Test isWord method work well.
+   */
+  @Test
+  public void testIsword() {
+    SentenceElement w = new PunctuationElement("!");
+    assertFalse(w.isWord());
 
+    w = new PunctuationElement(",");
+    assertFalse(w.isWord());
+
+    w = new PunctuationElement(".");
+    assertFalse(w.isWord());
+
+  }
+
+  /**
+   * Test duplicate method work well.
+   */
   @Test
   public void testDuplicate() {
     SentenceElement s = new PunctuationElement("!");
--- lab05/test/WordElementTest.java
+++ lab05/test/WordElementTest.java
@@ -1,23 +1,37 @@
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+/**
+ * Test WordElementTest method.
+ * 
+ * @author eddie
+ *
+ */
 public class WordElementTest {
 
+  /**
+   * Test isWord work well.
+   */
   @Test
-  public void test() {
+  public void testIsword() {
     WordElement w = new WordElement("Hello");
     assertTrue(w.isWord());
-    
+
     w = new WordElement("GoodBye");
     assertTrue(w.isWord());
 
-    
     w = new WordElement(".");
     assertTrue(w.isWord());
 
   }
-  
+
+  /**
+   * Test duplicate method work well.
+   */
   @Test
   public void testDuplicate() {
     SentenceElement s = new WordElement("Hello");
