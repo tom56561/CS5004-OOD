a8f134b79dba16f37b3c64bc96c931588804e288
tom56561 <tom56561@gmail.com>
2022-11-15 14:20:31 -0500

    add getBoard

--- Lab06/src/cs5004/tictactoe/TicTacToeModel.java
+++ Lab06/src/cs5004/tictactoe/TicTacToeModel.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class TicTacToeModel implements TicTacToe {
 
   private Player[][] board;
-    
+
   public TicTacToeModel() {
     this.board = new Player[3][3];
   }
@@ -14,27 +14,19 @@ public class TicTacToeModel implements TicTacToe {
   @Override
   public String toString() {
     // Using Java stream API to save code:
-    return Arrays.stream(getBoard()).map(
-      row -> " " + Arrays.stream(row).map(
-        p -> p == null ? " " : p.toString()).collect(Collectors.joining(" | ")))
-          .collect(Collectors.joining("\n-----------\n"));
-    // This is the equivalent code as above, but using iteration, and still using 
+    return Arrays
+        .stream(getBoard()).map(row -> " " + Arrays.stream(row)
+            .map(p -> p == null ? " " : p.toString()).collect(Collectors.joining(" | ")))
+        .collect(Collectors.joining("\n-----------\n"));
+    // This is the equivalent code as above, but using iteration, and still using
     // the helpful built-in String.join method.
     /**********
-    List<String> rows = new ArrayList<>();
-    for(Player[] row : getBoard()) {
-      List<String> rowStrings = new ArrayList<>();
-      for(Player p : row) {
-        if(p == null) {
-          rowStrings.add(" ");
-        } else {
-          rowStrings.add(p.toString());
-        }
-      }
-      rows.add(" " + String.join(" | ", rowStrings));
-    }
-    return String.join("\n-----------\n", rows);
-    ************/
+     * List<String> rows = new ArrayList<>(); for(Player[] row : getBoard()) {
+     * List<String> rowStrings = new ArrayList<>(); for(Player p : row) { if(p ==
+     * null) { rowStrings.add(" "); } else { rowStrings.add(p.toString()); } }
+     * rows.add(" " + String.join(" | ", rowStrings)); } return
+     * String.join("\n-----------\n", rows);
+     ************/
   }
 
   @Override
@@ -42,31 +34,31 @@ public class TicTacToeModel implements TicTacToe {
     if (this.isGameOver()) {
       throw new IllegalArgumentException("Cannot make a move after the game is over");
     }
-    if(r < 0 || c < 0 || r > 2 || c > 2) {
+    if (r < 0 || c < 0 || r > 2 || c > 2) {
       throw new IllegalArgumentException("Cannot get a mark that is off the board.");
     }
-    
-    if (this.getMarkAt(r, c) == Player.X || this.getMarkAt(r, c) == Player.O ) {
+
+    if (this.getMarkAt(r, c) == Player.X || this.getMarkAt(r, c) == Player.O) {
       throw new IllegalArgumentException("Cannot move to a spot that is already occupied.");
     }
     this.board[r][c] = this.getTurn();
   }
 
   @Override
-  public Player getTurn() {  
+  public Player getTurn() {
     int xTotal = 0;
     int oTotal = 0;
-    
+
     for (int row = 0; row < 3; row++) {
       for (int col = 0; col < 3; col++) {
         if (this.getMarkAt(row, col) == Player.X) {
           xTotal++;
-        }else if(this.getMarkAt(row, col) == Player.O) {
+        } else if (this.getMarkAt(row, col) == Player.O) {
           oTotal++;
         }
       }
     }
-    
+
     if (oTotal < xTotal) {
       return Player.O;
     }
@@ -87,12 +79,22 @@ public class TicTacToeModel implements TicTacToe {
 
   @Override
   public Player[][] getBoard() {
-    return this.board;
+
+    return Arrays.stream(this.board).map(row -> row.clone()).toArray(b -> board.clone());
+    
+        
+//    Player[][] bd = new Player[3][3];
+//    for (int row = 0; row < 3; row++) {
+//      for (int col = 0; col < 3; col++) {
+//        bd[row][col] = this.board[row][col];
+//      }
+//    }
+//    return bd;
   }
 
   @Override
   public Player getMarkAt(int r, int c) throws IllegalArgumentException {
-    if(r < 0 || c < 0 || r > 2 || c > 2) {
+    if (r < 0 || c < 0 || r > 2 || c > 2) {
       throw new IllegalArgumentException("Cannot get a mark that is off the board");
     }
     return this.board[r][c];
