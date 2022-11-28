19f81ffbbda1c9ef86f8dab0007e012ca21a9769
tom56561 <tom56561@gmail.com>
2022-11-28 16:36:28 -0500

    animation interface enum

---
+++ Animation/src/model/InterfaceShape.java
@@ -0,0 +1,90 @@
+package model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+/**
+ * Shape Interface defines all the supported operations for each shape.
+ * 
+ * @author eddie
+ *
+ */
+public interface InterfaceShape {
+  /**
+   * Return the shape type.
+   * 
+   * @return the shape type
+   */
+  ShapeType getType();
+
+  /**
+   * Get the color of shape.
+   * 
+   * @return the color of shape
+   */
+  Color getColor();
+
+  /**
+   * Get the width of the shape.
+   * 
+   * @return the width of the shape
+   */
+  int getWidth();
+
+  /**
+   * Get height of the shape.
+   * 
+   * @return height of the shape
+   */
+  int getHeight();
+
+  /**
+   * Get the position of the shape.
+   * 
+   * @return the position of the shape
+   */
+  Point2D getPosition();
+
+  /**
+   * Get the time of shape will appear.
+   * 
+   * @return the time of shape will appear
+   */
+  int getAppear();
+
+  /**
+   * Get the time of shape will disappear.
+   * 
+   * @return the time of shape will disappear
+   */
+  int getDisappear();
+
+  /**
+   * Set the position of the shape by given Point2D.
+   * 
+   * @param point include x y of the position as a Point2D type
+   */
+  void setPosition(Point2D point);
+
+  /**
+   * Set the color of the shape by given color.
+   * 
+   * @param color is RGB color
+   */
+  void setColor(Color color);
+
+  /**
+   * Set the width of the shape by given width
+   * 
+   * @param w the width that the shape should be changed to
+   */
+  void setWidth(int w);
+
+  /**
+   * Set the height of the shape by given height
+   * 
+   * @param h the height that the shape should be changed to
+   */
+  void setHeight(int h);
+
+}
---
+++ Animation/src/model/ShapeType.java
@@ -0,0 +1,8 @@
+package model;
+
+/*
+ * The emun includes rectangle, oval two type.
+ */
+public enum ShapeType {
+  rectangle, oval
+}
